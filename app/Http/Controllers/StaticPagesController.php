7ac9dd80e2dab82240248aa60ecb87d7fcf410d0
EwenSal <wubofeng@vip.qq.com>
2018-06-21 14:54:08 +0000

    Finish layout an routes

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //主页
+    public function home()
+    {
+    	return view('static_pages/home');
+    }
+    //关于
+    public function about()
+    {
+    	return view('static_pages/about');
+    }
+    //帮助
+    public function help()
+    {
+    	return view('static_pages/help');
+    }
+}
